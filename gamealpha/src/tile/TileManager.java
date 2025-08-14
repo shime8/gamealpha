7ae491807d5bf364b9e02cd96a4cefdb95e58efb
SZYMINPC\Szymon <szymeksarna@gmail.com>
2025-08-15 00:57:36 +0200

    qsave

---
+++ gamealpha/src/tile/TileManager.java
@@ -0,0 +1,30 @@
+package tile;
+
+import main.GamePanel;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class TileManager {
+    GamePanel gp;
+    Tile[] tile;
+
+    public TileManager(GamePanel gp) {
+
+        this.gp = gp;
+
+        tile = new Tile[10];
+
+        getTileImage();
+    }
+
+    public void getTileImage(){
+
+        try{
+            tile[0] = new Tile();
+            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass.png"));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
