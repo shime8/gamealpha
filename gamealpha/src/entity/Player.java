1cfb189d3f430de0c1aeecae16a6ac61fbd1e925
SZYMINPC\Szymon <szymeksarna@gmail.com>
2025-08-15 14:38:00 +0200

    tutorial 5 mapa wieksza niz ekran

--- gamealpha/src/entity/Player.java
+++ gamealpha/src/entity/Player.java
@@ -10,25 +10,31 @@ public class Player extends Entity{
     GamePanel gp;
     KeyHandler keyH;
 
+    public final int screenX;
+    public final int screenY;
+
     public Player(GamePanel gp, KeyHandler keyH){
         this.gp = gp;
         this.keyH = keyH;
 
+        screenX = (gp.screenWidth - gp.tileSize)/2;
+        screenY = (gp.screenHeight - gp.tileSize)/2;
+
         setDefaultValues();
     }
     public void setDefaultValues(){
-        x = 100;
-        y = 100;
-        speed = 4;
+        worldX = gp.tileSize*22;
+        worldY = gp.tileSize*22;
+        speed = 6;
     }
     public void update(){
-        y -= keyH.upPressed ? speed : 0;
-        y  += keyH.downPressed ? speed : 0;
-        x  -= keyH.leftPressed ? speed : 0;
-        x += keyH.rightPressed ? speed : 0;
+        worldY -= keyH.upPressed ? speed : 0;
+        worldY += keyH.downPressed ? speed : 0;
+        worldX -= keyH.leftPressed ? speed : 0;
+        worldX += keyH.rightPressed ? speed : 0;
     }
     public void draw(Graphics2D g2){
         g2.setColor(Color.white);
-        g2.fillRect(x,y,gp.tileSize, gp.tileSize);
+        g2.fillRect(screenX, screenY,gp.tileSize, gp.tileSize);
     }
 }
--- gamealpha/src/main/GamePanel.java
+++ gamealpha/src/main/GamePanel.java
@@ -18,11 +18,16 @@ public class GamePanel extends JPanel implements Runnable{
     public int screenWidth = tileSize * maxScreenCol; // 1024
     public int screenHeight = tileSize * maxScreenRow; // 768
 
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldWidth = tileSize * maxWorldCol;
+    public final int worldHeight = tileSize * maxWorldRow;
+
     // FPS
     int FPS = 60;
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
-    Player player = new Player(this,keyH);
+    public Player player = new Player(this,keyH);
     TileManager tileM = new TileManager(this);
 
 
